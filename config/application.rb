78d736075d78e65d47d52473792a0487e13563fb
mrmrtmrn <mrmrtmrn@gmail.com>
2024-01-17 00:12:27 +0900

    RSpec周りの設定追加

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,15 @@ module LoginWithDevice
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # RSpec configuration
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+      g.factory_bot false
+    end
   end
 end
